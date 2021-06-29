8daf06f3b7b7f44f2d4b71d66716277df8021a3b
Aynur Galimov <galimovaynur@protonmail.com>
2021-06-29 14:22:57 +0500

    add dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
